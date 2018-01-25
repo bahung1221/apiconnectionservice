f58fa38081c3d6ce7995a11157f8160e6f2a44b6
bahung1221 <bahung1221@gmail.com>
2018-01-25 13:08:08 +0700

    init commit

---
+++ src/Helper.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Hungnguyen\Apiconnectionservice;
+
+use Hungnguyen\Curl\Curl;
+
+trait Helper
+{
+    protected $curl;
+
+    protected function get(string $api, array $input = [], $isGetMetaData = false) : array
+    {
+        return $this->getCurl()->get($api, $this->isValid($input), $isGetMetaData);
+    }
+
+    protected function post(string $api, array $input = []) : array
+    {
+        return $this->getCurl()->post($api, $this->isValid($input));
+    }
+
+    protected function put(string $api, string $ids, array $input = []) : array
+    {
+        return $this->getCurl()->put($api, $ids, $this->isValid($input));
+    }
+
+    // protected function delete(string $api, int $id)
+    // {
+    //     $this->getCurl()->delete($api, $id);
+    // }
+
+    protected function getCurl()
+    {
+        if (empty($this->curl)) {
+            $this->curl = new Curl($this->config['host']);
+        }
+        return $this->curl;
+    }
+
+    protected function initCurl(string $url)
+    {
+        unset($this->curl);
+        $this->curl = new Curl($url);
+        return $this;
+    }
+
+    protected function isValid(array &$input) : array
+    {
+        if (is_array(head($input))) {
+            foreach ($input as $key => $record) {
+                if (is_null($input[$key]['is_valid'] ?? null)) {
+                    $input[$key]['is_valid'] = 1;
+                }
+            }
+        } elseif (is_null($input['is_valid'] ?? null)) {
+            $input['is_valid'] = 1;
+        }
+        return $input;
+    }
+
+    protected function setTag(string $tag)
+    {
+        $this->getCurl()->setTag($tag);
+        return $this;
+    }
+}
---
+++ src/Service.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Hungnguyen\Apiconnectionservice;
+
+use Hungnguyen\Apiconnectionservice\Helper;
+
+trait Service
+{
+    use Helper;
+
+    protected $model;
+
+    protected $isPaging = false;
+    protected $page = 0;
+    protected $lastPage = 0;
+
+    public function setPaging(bool $val)
+    {
+        $this->isPaging = $val;
+        return $this;
+    }
+
+    protected function paging(array $data, int $firstPage, int $lastPage) : array
+    {
+        return $this->isPaging
+            ? Utils::calculatePaging(array_null_filter($data), $firstPage, $lastPage)
+            : array_null_filter($data);
+    }
+
+    public function setModel(string $model)
+    {
+        $this->model = $model;
+        return $this;
+    }
+
+    public function getModel()
+    {
+        return class_exists($this->model) ? new $this->model : null;
+    }
+
+    public function setCurl(string $url)
+    {
+        return $this->initCurl($url);
+    }
+}
