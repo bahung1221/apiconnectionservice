f37ae4f1491b6974b4be3301586c8350fdf97a89
bahung1221 <bahung1221@gmail.com>
2018-02-01 17:41:31 +0700

    Set tag & header

--- src/Helper.php
+++ src/Helper.php
@@ -8,24 +8,47 @@ trait Helper
 {
     protected $curl;
 
+    protected function getTag(array $input = []) : string
+    {
+        $tag = $input['tag'] ?? '';
+        unset($input['tag']);
+        return $tag;
+    }
+
     protected function get(string $api, array $input = [], $isGetMetaData = false) : array
     {
-        return $this->getCurl()->get($api, $this->isValid($input), $isGetMetaData);
+        $tag = $this->getTag($input);
+
+        return $this->getCurl()
+                    ->setSpecialTag($tag)
+                    ->get($api, $this->isValid($input), $isGetMetaData);
     }
 
     protected function post(string $api, array $input = []) : array
     {
-        return $this->getCurl()->post($api, $this->isValid($input));
+        $tag = $this->getTag($input);
+
+        return $this->getCurl()
+                    ->setSpecialTag($tag)
+                    ->post($api, $this->isValid($input));
     }
 
     protected function put(string $api, $ids, array $input = []) : array
     {
-        return $this->getCurl()->put($api, $ids, $this->isValid($input));
+        $tag = $this->getTag($input);
+
+        return $this->getCurl()
+                    ->setSpecialTag($tag)
+                    ->put($api, $ids, $this->isValid($input));
     }
 
     protected function deleteById(string $api, int $id)
     {
-        $this->getCurl()->delete($api, $id);
+        $tag = $this->getTag($input);
+
+        $this->getCurl()
+            ->setSpecialTag($tag)
+            ->delete($api, $id);
     }
 
     protected function getCurl()
@@ -55,11 +78,5 @@ trait Helper
             $input['is_valid'] = 1;
         }
         return $input;
-    }
-
-    protected function setTag(string $tag)
-    {
-        $this->getCurl()->setTag($tag);
-        return $this;
     }
 }
